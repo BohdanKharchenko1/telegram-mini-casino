3ed80a06d236a2064a5bcf0e5e32027351c7e2a0
Bohdan <84513013+naughtyCURSED@users.noreply.github.com>
2025-10-19 22:00:59 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import WebApp from "@twa-dev/sdk";
 import {useEffect} from "react";
 import { useRawInitData } from '@telegram-apps/sdk-react';
 import { loginWithTelegram } from './misc/AuthService.ts';
+import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react';
 
 export default function App(){
     console.log("App is rendering!");
@@ -27,12 +28,17 @@ export default function App(){
 
 
         return (
+          <TonConnectUIProvider manifestUrl='https://telegram-mini-casino.vercel.app/tonconnect-manifest.json'>
           <div className="min-h-screen flex items-center justify-center bg-gray-200">
             <button className="h-12 w-32 bg-blue-500 text-white rounded" onClick={handleClick}>
                 Click me!
             </button>
 
+
+              <TonConnectButton />
+
         </div>
+          </TonConnectUIProvider>
         );
 
 }
