7db353f7a321ffd7ff6652c01ce63f704599a9e1
Bohdan <84513013+naughtyCURSED@users.noreply.github.com>
2025-10-20 23:54:52 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import WebApp from "@twa-dev/sdk";
 import {useEffect} from "react";
-import { useRawInitData } from '@telegram-apps/sdk-react';
 import { loginWithTelegram } from './misc/AuthService.ts';
-import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react';
+import {  useTonConnectUI } from '@tonconnect/ui-react';
 
 export default function App(){
-    console.log("App is rendering!");
-    const info = useRawInitData();
 
-    const handleClick = () => {
-        if (info) {
-            const params = new URLSearchParams(info);
-            const userJson = params.get("user");
-            if (userJson) {
-                const user = JSON.parse(decodeURIComponent(userJson));
-                console.log("Decoded user:", user);
-            }
-        }
-    }
-
+    const [tonConnectUI] = useTonConnectUI();
     useEffect(()=> {
 
         WebApp.expand();
@@ -28,17 +15,11 @@ export default function App(){
 
 
         return (
-          <TonConnectUIProvider manifestUrl='https://telegram-mini-casino.vercel.app/tonconnect-manifest.json'>
-          <div className="min-h-screen flex items-center justify-center bg-gray-200">
-            <button className="h-12 w-32 bg-blue-500 text-white rounded" onClick={handleClick}>
-                Click me!
-            </button>
-
-
-              <TonConnectButton />
-
-        </div>
-          </TonConnectUIProvider>
+              <div className="flex min-h-screen items-center justify-center w-full">
+                    <button className=' bg-blue-400' onClick={()=>tonConnectUI.openModal()}>
+                        connect!
+                    </button>
+              </div>
         );
 
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import './index.css'
 import App from "./App.tsx";
 import WebApp from "@twa-dev/sdk";
 import Footer from "./parts/Footer.tsx";
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 
 WebApp.ready();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <TonConnectUIProvider
+      manifestUrl="https://telegram-mini-casino.vercel.app/tonconnect-manifest.json"
+    >
   <App/>
   <Footer/>
+    </TonConnectUIProvider>
   </StrictMode>,
 )
