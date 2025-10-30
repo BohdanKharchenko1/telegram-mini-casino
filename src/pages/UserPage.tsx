e64d533dde023f10af7233e57183a5c0d919db55
Bohdan <84513013+naughtyCURSED@users.noreply.github.com>
2025-10-30 02:50:50 +0100

    stuff

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,5 +1,5 @@
 import Paper from '@mui/material/Paper';
-import { Avatar, Box, Stack } from '@mui/material';
+import { Avatar, Box, Stack, Typography } from '@mui/material';
 import NavBar from '../components/NavBar.tsx';
 import { useEffect } from 'react';
 import WebApp from '@twa-dev/sdk';
@@ -43,8 +43,80 @@ export default function UserPage() {
         </Stack>
 
         {/* Buttons row */}
+      </Paper>
+
+
+      <Paper
+        sx={{
+          position: 'absolute',
+          top: 200,
+          left: '50%',
+          transform: 'translateX(-50%)',
+          width: '85%',
+          maxWidth: 500,
+          borderRadius: '24px',
+          background:
+            'linear-gradient(27deg, rgba(255,79,160,0.2) 0%, rgba(182,66,154,0.2) 50%, rgba(115,46,145,0.2) 100%)',
+          boxShadow: '0 0 25px rgba(255, 100, 200, 0.2)',
+          border: '1px solid rgba(255, 255, 255, 0.1)',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          p: 3,
+          gap: 2,
+          color: '#fff',
+        }}
+        elevation={0}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'baseline',
+            justifyContent: 'left',
+            gap: 1.5,
+            width: '100%',
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: '18px',
+              color: '#d5baff',
+              fontWeight: 500,
+            }}
+          >
+            Баланс
+          </Typography>
+
+          <Typography
+            sx={{
+              fontSize: '36px',
+              fontWeight: 700,
+              color: '#FFD84D',
+              textShadow: '0 0 18px rgba(255, 216, 77, 0.6)',
+            }}
+          >
+            {Number(user?.balance ?? 0).toFixed(2)}
+          </Typography>
+
+          <Typography
+            sx={{
+              fontSize: '22px',
+              fontWeight: 600,
+              color: '#FFD84D',
+              textShadow: '0 0 12px rgba(255, 216, 77, 0.6)',
+            }}
+          >
+            TON
+          </Typography>
+        </Box>
+
         <TonConnectButton tonConnect={tonConnectUI} userId={`${user?.id}`} />
       </Paper>
+
+
+
+
 
       <NavBar />
     </Box>
