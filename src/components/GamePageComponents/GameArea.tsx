d37608843f2967fbd287597128d5732f8589471f
Bohdan <84513013+naughtyCURSED@users.noreply.github.com>
2026-01-11 05:55:31 +0100

    game page

---
+++ src/components/GamePageComponents/GameArea.tsx
@@ -0,0 +1,27 @@
+import Paper from '@mui/material/Paper';
+
+export default function GameArea() {
+  return (
+    <Paper
+      sx={{
+        position: 'absolute',
+        top: 80,
+        left: '50%',
+        height: '35vh',
+        transform: 'translateX(-50%)',
+        width: '80%',
+        maxWidth: 500,
+        borderRadius: 6,
+        background:
+          'linear-gradient(27deg, rgba(255,79,160,0.6) 0%, rgba(182,66,154,0.6) 50%, rgba(115,46,145,0.6) 100%)',
+        display: 'flex',
+        flexDirection: 'column',
+        p: 3,
+        gap: 2,
+      }}
+      elevation={2}
+    >
+
+    </Paper>
+  )
+}
--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,9 +1,30 @@
-import { Box } from '@mui/material';
+import { Avatar, Box } from '@mui/material';
+import { useStore } from '../misc/store.ts';
+import GameArea from '../components/GamePageComponents/GameArea.tsx';
+import BettingArea from '../components/GamePageComponents/BettingArea.tsx';
 
 export default function GamePage() {
+  const user = useStore((state) =>state.user)
   return (
     <Box sx={{background: '#2b1436', height: '100vh', width: '100%'}}>
-        hello
+        <Box
+          sx={{
+            position: 'absolute',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            width: '90%',
+            maxWidth: 500,
+            display: 'flex',
+            alignItems: 'center',
+            p: 3,
+            gap: 2,
+          }}
+        >
+          <Avatar src={user?.photoUrl || undefined} />
+          <h2 className='text-white'>@{user?.username}</h2>
+        </Box>
+        <GameArea/>
+        <BettingArea/>
     </Box>
   )
 }
